640dd8b553f217f45b36bfafb9be324849f341e4
JhoanDucu <114232996+JhoanDucu@users.noreply.github.com>
2023-08-13 10:31:40 -0500

    Add files via upload

---
+++ HTTPS/SopaletrasGame/main.js
@@ -0,0 +1,75 @@
+let palabra = [];
+let buttons = [];
+let porBuscar = {
+   'SSL': false,
+   'COMUNICACION': false,
+   'PROTOCOLO': false,
+   'CONEXION': false,
+   'CERTIFICADO': false,
+   'NAVEGADOR': false,
+   'ENCRIPTACION': false,
+   'PRIVACIDAD': false,
+   'SEGURIDAD': false,
+   'URL': false,
+   'HTTP': false,
+   'HTTPS': false,
+}
+let li = {};
+let encontradas = 0
+function empiezaSopa(
+   characters ='ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'
+   ){
+   let matriz = document.getElementById('matriz');
+   for (let rows = 0; rows < 14; rows++) {
+      let tr = document.createElement('tr');
+      matriz.appendChild(tr);
+      for (let column = 0; column < 14; column++) {
+         let td = document.createElement('td');
+         tr.appendChild(td);
+         let c = document.createElement('button');
+         td.appendChild(c);
+         
+         c.type = 'button';
+         c.className = 'btn btn-outline-light';
+         c.innerHTML = characters.charAt(Math.random() * 27);
+         c.id = c.innerHTML + rows + column;
+         c.setAttribute('onclick','crearPalabra('+c.id+');');
+      }
+      const oneLi = document.getElementById('i'+rows);
+      if (oneLi) {
+         li[oneLi.innerHTML] = oneLi.id;
+      }
+   }
+}
+function crearPalabra(button){
+   button.setAttribute("disabled", "");
+   palabra.push(button.innerHTML);
+   buttons.push(button);
+}
+function buscarPalabra(){
+   let word = palabra.length > 0 ? palabra.reduce((bef, now) => {
+      return bef += now;
+   }) : alert('No has ingresado palabra');
+   
+   if (porBuscar.hasOwnProperty(word)) {
+      porBuscar[word] = true;
+      const tachar = document.getElementById(li[word]);
+      tachar.setAttribute('style', 'text-decoration: line-through;');
+      encontradas++;
+      for (const validar in porBuscar) {
+         if (!porBuscar[validar]) {
+            noPalabra();
+            break;
+         }
+         if (encontradas === 12) {
+            alert('Has armado la sopa de letras');
+            window.location = ''; // COLOCAR AQUI ADENTRO A DONDE LO QUIERES REDIRIGUIR AL COMPLETAR EL JUEGO
+         }
+      }
+   }
+}
+function noPalabra(palabraAgain = [], buttonsAgain = []) {
+   buttons.forEach((each) => each.removeAttribute('disabled'));
+   palabra = palabraAgain;
+   buttons = buttonsAgain;
+}
---
+++ HTTPS/crucigramaGame/Crucigrama.js
@@ -0,0 +1,448 @@
+for(fila = 1; fila <= 18; fila++){
+    for(columna = 1; columna <= 18; columna++){
+        document.getElementById("F" + fila + "C" + columna).readOnly=true;
+    }
+}
+
+let p1_l1 = document.getElementById("F3C17");
+let p1_l2 = document.getElementById("F4C17");
+let p1_l3 = document.getElementById("F5C17");
+
+let p2_l1 = document.getElementById("F4C12");
+let p2_l2 = document.getElementById("F5C12");
+let p2_l3 = document.getElementById("F6C12");
+let p2_l4 = document.getElementById("F7C12");
+let p2_l5 = document.getElementById("F8C12");
+let p2_l6 = document.getElementById("F9C12");
+let p2_l7 = document.getElementById("F10C12");
+let p2_l8 = document.getElementById("F11C12");
+let p2_l9 = document.getElementById("F12C12");
+let p2_l10 = document.getElementById("F13C12");
+let p2_l11 = document.getElementById("F14C12");
+let p2_l12 = document.getElementById("F15C12");
+
+let p3_l1 = document.getElementById("F5C10");
+let p3_l2 = document.getElementById("F5C11");
+let p3_l3 = document.getElementById("F5C12");
+let p3_l4 = document.getElementById("F5C13");
+let p3_l5 = document.getElementById("F5C14");
+let p3_l6 = document.getElementById("F5C15");
+let p3_l7 = document.getElementById("F5C16");
+let p3_l8 = document.getElementById("F5C17");
+let p3_l9 = document.getElementById("F5C18");
+
+let p4_l1 = document.getElementById("F8C10");
+let p4_l2 = document.getElementById("F8C11");
+let p4_l3 = document.getElementById("F8C12");
+let p4_l4 = document.getElementById("F8C13");
+let p4_l5 = document.getElementById("F8C14");
+let p4_l6 = document.getElementById("F8C15");
+let p4_l7 = document.getElementById("F8C16");
+let p4_l8 = document.getElementById("F8C17");
+
+let p5_l1 = document.getElementById("F10C5");
+let p5_l2 = document.getElementById("F10C6");
+let p5_l3 = document.getElementById("F10C7");
+let p5_l4 = document.getElementById("F10C8");
+let p5_l5 = document.getElementById("F10C9");
+let p5_l6 = document.getElementById("F10C10");
+let p5_l7 = document.getElementById("F10C11");
+let p5_l8 = document.getElementById("F10C12");
+let p5_l9 = document.getElementById("F10C13");
+let p5_l10 = document.getElementById("F10C14");
+let p5_l11 = document.getElementById("F10C15");
+
+let p6_l1 = document.getElementById("F8C17");
+let p6_l2 = document.getElementById("F9C17");
+let p6_l3 = document.getElementById("F10C17");
+let p6_l4 = document.getElementById("F11C17");
+let p6_l5 = document.getElementById("F12C17");
+let p6_l6 = document.getElementById("F13C17");
+let p6_l7 = document.getElementById("F14C17");
+let p6_l8 = document.getElementById("F15C17");
+let p6_l9 = document.getElementById("F16C17");
+
+let p7_l1 = document.getElementById("F7C7");
+let p7_l2 = document.getElementById("F8C7");
+let p7_l3 = document.getElementById("F9C7");
+let p7_l4 = document.getElementById("F10C7");
+let p7_l5 = document.getElementById("F11C7");
+let p7_l6 = document.getElementById("F12C7");
+let p7_l7 = document.getElementById("F13C7");
+let p7_l8 = document.getElementById("F14C7");
+let p7_l9 = document.getElementById("F15C7");
+let p7_l10 = document.getElementById("F16C7");
+let p7_l11 = document.getElementById("F17C7");
+let p7_l12 = document.getElementById("F18C7");
+
+let p8_l1 = document.getElementById("F5C5");
+let p8_l2 = document.getElementById("F6C5");
+let p8_l3 = document.getElementById("F7C5");
+let p8_l4 = document.getElementById("F8C5");
+let p8_l5 = document.getElementById("F9C5");
+let p8_l6 = document.getElementById("F10C5");
+let p8_l7 = document.getElementById("F11C5");
+let p8_l8 = document.getElementById("F12C5");
+let p8_l9 = document.getElementById("F13C5");
+let p8_l10 = document.getElementById("F14C5");
+
+let p9_l1 = document.getElementById("F16C2");
+let p9_l2 = document.getElementById("F16C3");
+let p9_l3 = document.getElementById("F16C4");
+let p9_l4 = document.getElementById("F16C5");
+let p9_l5 = document.getElementById("F16C6");
+let p9_l6 = document.getElementById("F16C7");
+let p9_l7 = document.getElementById("F16C8");
+let p9_l8 = document.getElementById("F16C9");
+let p9_l9 = document.getElementById("F16C10");
+
+let p10_l1 = document.getElementById("F16C5");
+let p10_l2 = document.getElementById("F17C5");
+let p10_l3 = document.getElementById("F18C5");
+
+let p11_l1 = document.getElementById("F12C2");
+let p11_l2 = document.getElementById("F13C2");
+let p11_l3 = document.getElementById("F14C2");
+let p11_l4 = document.getElementById("F15C2");
+let p11_l5 = document.getElementById("F16C2");
+
+let p12_l1 = document.getElementById("F2C10");
+let p12_l2 = document.getElementById("F3C10");
+let p12_l3 = document.getElementById("F4C10");
+let p12_l4 = document.getElementById("F5C10");
+
+p1_l1.readOnly = false;
+p1_l2.readOnly = false;
+p1_l3.readOnly = false;
+
+p2_l1.readOnly = false;
+p2_l2.readOnly = false;
+p2_l3.readOnly = false;
+p2_l4.readOnly = false;
+p2_l5.readOnly = false;
+p2_l6.readOnly = false;
+p2_l7.readOnly = false;
+p2_l8.readOnly = false;
+p2_l9.readOnly = false;
+p2_l10.readOnly = false;
+p2_l11.readOnly = false;
+p2_l12.readOnly = false;
+
+p3_l1.readOnly = false;
+p3_l2.readOnly = false;
+p3_l3.readOnly = false;
+p3_l4.readOnly = false;
+p3_l5.readOnly = false;
+p3_l6.readOnly = false;
+p3_l7.readOnly = false;
+p3_l8.readOnly = false;
+p3_l9.readOnly = false;
+
+p4_l1.readOnly = false;
+p4_l2.readOnly = false;
+p4_l3.readOnly = false;
+p4_l4.readOnly = false;
+p4_l5.readOnly = false;
+p4_l6.readOnly = false;
+p4_l7.readOnly = false;
+p4_l8.readOnly = false;
+
+p5_l1.readOnly = false;
+p5_l2.readOnly = false;
+p5_l3.readOnly = false;
+p5_l4.readOnly = false;
+p5_l5.readOnly = false;
+p5_l6.readOnly = false;
+p5_l7.readOnly = false;
+p5_l8.readOnly = false;
+p5_l9.readOnly = false;
+p5_l10.readOnly = false;
+p5_l11.readOnly = false;
+
+p6_l1.readOnly = false;
+p6_l2.readOnly = false;
+p6_l3.readOnly = false;
+p6_l4.readOnly = false;
+p6_l5.readOnly = false;
+p6_l6.readOnly = false;
+p6_l7.readOnly = false;
+p6_l8.readOnly = false;
+p6_l9.readOnly = false;
+
+p7_l1.readOnly = false;
+p7_l2.readOnly = false;
+p7_l3.readOnly = false;
+p7_l4.readOnly = false;
+p7_l5.readOnly = false;
+p7_l6.readOnly = false;
+p7_l7.readOnly = false;
+p7_l8.readOnly = false;
+p7_l9.readOnly = false;
+p7_l10.readOnly = false;
+p7_l11.readOnly = false;
+p7_l12.readOnly = false;
+
+p8_l1.readOnly = false;
+p8_l2.readOnly = false;
+p8_l3.readOnly = false;
+p8_l4.readOnly = false;
+p8_l5.readOnly = false;
+p8_l6.readOnly = false;
+p8_l7.readOnly = false;
+p8_l8.readOnly = false;
+p8_l9.readOnly = false;
+p8_l10.readOnly = false;
+
+p9_l1.readOnly = false;
+p9_l2.readOnly = false;
+p9_l3.readOnly = false;
+p9_l4.readOnly = false;
+p9_l5.readOnly = false;
+p9_l6.readOnly = false;
+p9_l7.readOnly = false;
+p9_l8.readOnly = false;
+p9_l9.readOnly = false;
+
+p10_l1.readOnly = false;
+p10_l2.readOnly = false;
+p10_l3.readOnly = false;
+
+p11_l1.readOnly = false;
+p11_l2.readOnly = false;
+p11_l3.readOnly = false;
+p11_l4.readOnly = false;
+p11_l5.readOnly = false;
+
+p12_l1.readOnly = false;
+p12_l2.readOnly = false;
+p12_l3.readOnly = false;
+p12_l4.readOnly = false;
+
+
+for(fila = 1; fila <= 18; fila++){
+    for(columna = 1; columna <= 18; columna++){
+        if(document.getElementById("F" + fila + "C" + columna).readOnly == false){
+            document.getElementById("F" + fila + "C" + columna).style.backgroundColor="cyan";
+        }
+    }
+}
+
+let errorAct = 0;
+function error(){
+    document.getElementById("msg").innerHTML="¡Hay palabras incorrectas o faltan palabras!";
+    document.getElementById("msg").style.backgroundColor="cyan";
+    document.getElementById("msg").style.width="200px";
+    errorAct = 1;
+}
+
+setInterval('ocultarError()',5000);
+
+function ocultarError(){
+    if(errorAct == 1){
+        document.getElementById("msg").innerHTML="";
+        document.getElementById("msg").style;
+        errorAct = 0;
+    }
+}
+
+function validar(){
+    document.getElementById("msg").innerHTML="";
+    p1 = p1_l1.value + p1_l2.value + p1_l3.value;
+    p2 = p2_l1.value + p2_l2.value + p2_l3.value + p2_l4.value + p2_l5.value + p2_l6.value + p2_l7.value + p2_l8.value + p2_l9.value + p2_l10.value + p2_l11.value + p2_l12.value;
+    p3 = p3_l1.value + p3_l2.value + p3_l3.value + p3_l4.value + p3_l5.value + p3_l6.value + p3_l7.value + p3_l8.value + p3_l9.value;
+    p4 = p4_l1.value + p4_l2.value + p4_l3.value + p4_l4.value + p4_l5.value + p4_l6.value + p4_l7.value + p4_l8.value;
+    p5 = p5_l1.value + p5_l2.value + p5_l3.value + p5_l4.value + p5_l5.value + p5_l6.value + p5_l7.value + p5_l8.value + p5_l9.value + p5_l10.value + p5_l11.value;
+    p6 = p6_l1.value + p6_l2.value + p6_l3.value + p6_l4.value + p6_l5.value + p6_l6.value + p6_l7.value + p6_l8.value + p6_l9.value;
+    p7 = p7_l1.value + p7_l2.value + p7_l3.value + p7_l4.value + p7_l5.value + p7_l6.value + p7_l7.value + p7_l8.value + p7_l9.value + p7_l10.value + p7_l11.value + p7_l12.value;
+    p8 = p8_l1.value + p8_l2.value + p8_l3.value + p8_l4.value + p8_l5.value + p8_l6.value + p8_l7.value + p8_l8.value + p8_l9.value + p8_l10.value;
+    p9 = p9_l1.value + p9_l2.value + p9_l3.value + p9_l4.value + p9_l5.value + p9_l6.value + p9_l7.value + p9_l8.value + p9_l9.value;
+    p10 = p10_l1.value + p10_l2.value + p10_l3.value;
+    p11 = p11_l1.value + p11_l2.value + p11_l3.value + p11_l4.value + p11_l5.value;
+    p12= p12_l1.value + p12_l2.value + p12_l3.value + p12_l4.value;
+    
+    if(p1.toLowerCase()=="ssl" && p2.toLowerCase()=="comunicacion" && p3.toLowerCase()=="protocolo" && p4.toLowerCase()=="conexion" && p5.toLowerCase()=="certificado" && p6.toLowerCase()=="navegador" && p7.toLowerCase()=="encriptacion" && p8.toLowerCase()=="privacidad" && p9.toLowerCase()=="seguridad" && p10.toLowerCase()=="url" && p11.toLowerCase()=="https" && p12.toLowerCase()=="http"){
+        document.getElementById("msg").style.fontSize="20px";
+        document.getElementById("msg").innerHTML="¡Felicidades, has completado el crucigrama!";
+        window.location = '../SopaletrasGame/';
+    }
+    else{
+        if(p1.toLowerCase()!="ssl"){
+            p1_l1.value="";
+            p1_l2.value="";
+            p1_l3.value="";
+            error();
+        }
+        if(p2.toLowerCase()!="comunicacion"){
+            p2_l1.value="";
+            p2_l2.value="";
+            p2_l3.value="";
+            p2_l4.value="";
+            p2_l5.value="";
+            p2_l6.value="";
+            p2_l7.value="";
+            p2_l8.value="";
+            p2_l9.value="";
+            p2_l10.value="";
+            p2_l11.value="";
+            p2_l12.value="";
+            error();
+        }
+        if(p3.toLowerCase()!="protocolo"){
+            p3_l1.value="";
+            p3_l2.value="";
+            p3_l3.value="";
+            p3_l4.value="";
+            p3_l5.value="";
+            p3_l6.value="";
+            p3_l7.value="";
+            p3_l8.value="";
+            p3_l9.value="";
+            error();
+        }
+        if(p4.toLowerCase()!="conexion"){
+            p4_l1.value="";
+            p4_l2.value="";
+            p4_l3.value="";
+            p4_l4.value="";
+            p4_l5.value="";
+            p4_l6.value="";
+            p4_l7.value="";
+            p4_l8.value="";
+            error();
+        }
+        if(p5.toLowerCase()!="certificado"){
+            p5_l1.value="";
+            p5_l2.value="";
+            p5_l3.value="";
+            p5_l4.value="";
+            p5_l5.value="";
+            p5_l6.value="";
+            p5_l7.value="";
+            p5_l8.value="";
+            p5_l9.value="";
+            p5_l10.value="";
+            p5_l11.value="";
+            error();
+        }
+        if(p6.toLowerCase()!="navegador"){
+            p6_l1.value="";
+            p6_l2.value="";
+            p6_l3.value="";
+            p6_l4.value="";
+            p6_l5.value="";
+            p6_l6.value="";
+            p6_l7.value="";
+            p6_l8.value="";
+            p6_l9.value="";
+            error();
+        }
+        if(p7.toLowerCase()!="encriptacion"){
+            p7_l1.value="";
+            p7_l2.value="";
+            p7_l3.value="";
+            p7_l4.value="";
+            p7_l5.value="";
+            p7_l6.value="";
+            p7_l7.value="";
+            p7_l8.value="";
+            p7_l9.value="";
+            p7_l10.value="";
+            p7_l11.value="";
+            p7_l12.value="";
+            error();
+        }
+        if(p8.toLowerCase()!="privacidad"){
+            p8_l1.value="";
+            p8_l2.value="";
+            p8_l3.value="";
+            p8_l4.value="";
+            p8_l5.value="";
+            p8_l6.value="";
+            p8_l7.value="";
+            p8_l8.value="";
+            p8_l9.value="";
+            p8_l10.value="";
+            error();
+        }
+        if(p9.toLowerCase()!="seguridad"){
+            p9_l1.value="";
+            p9_l2.value="";
+            p9_l3.value="";
+            p9_l4.value="";
+            p9_l5.value="";
+            p9_l6.value="";
+            p9_l7.value="";
+            p9_l8.value="";
+            p9_l9.value="";
+            error();
+        }
+        if(p10.toLowerCase()!="url"){
+            p10_l1.value="";
+            p10_l2.value="";
+            p10_l3.value="";
+            error();
+        }
+        if(p11.toLowerCase()!="https"){
+            p11_l1.value="";
+            p11_l2.value="";
+            p11_l3.value="";
+            p11_l4.value="";
+            p11_l5.value="";
+            error();
+        }
+        if(p12.toLowerCase()!="http"){
+            p12_l1.value="";
+            p12_l2.value="";
+            p12_l3.value="";
+            p12_l4.value="";
+            error();
+        }
+
+        if(p1.toLowerCase()=="ssl"){
+            p1_l3.value="l";
+        }
+        if(p2.toLowerCase()=="comunicacion"){
+            p2_l2.value="o";
+            p2_l5.value="n";
+            p2_l7.value="c";
+        }
+        if(p3.toLowerCase()=="protocolo"){
+            p3_l1.value="p";
+            p3_l3.value="o";
+            p3_l8.value="l";
+        }
+        if(p4.toLowerCase()=="conexion"){
+            p4_l3.value="n";
+            p4_l8.value="n";
+        }
+        if(p5.toLowerCase()=="certificado"){
+            p5_l1.value="c";
+            p5_l3.value="r";
+            p5_l8.value="c";
+        }
+        if(p6.toLowerCase()=="navegador"){
+            p6_l1.value="n";
+        }
+        if(p7.toLowerCase()=="encriptacion"){
+            p7_l4.value="r";
+            p7_l10.value="i";
+        }
+        if(p8.toLowerCase()=="privacidad"){
+            p8_l6.value="c";
+        }
+        if(p9.toLowerCase()=="seguridad"){
+            p9_l1.value="s";
+            p9_l4.value="u";
+            p9_l6.value="i";
+        }
+        if(p10.toLowerCase()=="url"){
+            p10_l1.value="u";
+        }
+        if(p11.toLowerCase()=="https"){
+            p11_l5.value="s";
+        }
+        if(p12.toLowerCase()=="http"){
+            p12_l4.value="p";
+        }
+    }
+}
